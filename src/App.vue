a99523840bb75e735511e001e8884b424da204c8
Sydney Werbach <sydwer@gmail.com>
2020-04-03 21:36:17 -0600

    Added in the methods section to the script, clicking buttons now creates new buttons and adds to the counter

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   <div id="app">
     <div class="starter-info" v-if= "counter === 0">
       <h1>Howdy</h1>
-      <button @click="counter += 1">start</button>
+      <button v-on:click="counter += 1">start</button>
     </div>
     <h2>{{counter}}</h2>
-   
-    <button v-for="button in buttons" :key="button.id">
+  <div v-if="counter > 0" class="new-buttons"> 
+    <button v-on:click="addButton" v-for="button in buttons" :key="button.id">
       <RandomButton />
     </button>
+  </div>
 
   </div>
 </template>
@@ -26,15 +27,15 @@ export default {
        
       }]
     }
+  },
+  methods: {
+    addButton: function() {
+      this.counter += 1
+      this.buttons.push({type: "test", id: this.counter})
+    },
   }
 }
-// function addCounter(){
-//   return this.counter += 1
-// }
-// function addButton(){
-//   addCounter();
-//   buttons.push({type: "test", id: counter})
-// }
+
 
 </script>
 
---
+++ src/components/RandomButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <p> New Button</p>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
