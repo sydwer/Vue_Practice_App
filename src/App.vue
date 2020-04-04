dafdc0ccf6eccec3c8a119f034ed98bac0b2be21
Sydney Werbach <sydwer@gmail.com>
2020-04-03 20:43:22 -0600

    Added script section, and starting button to the data within export default

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
+    <h1>Howdy</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      buttons: [{
+        type: "start",
+      }]
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 
